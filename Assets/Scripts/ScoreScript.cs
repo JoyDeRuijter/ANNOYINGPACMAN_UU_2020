87185fb1fea91df37ca91748851eabf2b0155768
Siwertdj <siwert@live.nl>
2020-03-31 19:09:57 +0200

    fix rightborder, add nenew adverts, spawn adverts every 100 scorepoints

--- Assets/Scripts/ScoreScript.cs
+++ Assets/Scripts/ScoreScript.cs
@@ -8,22 +8,35 @@ public class ScoreScript : MonoBehaviour
 {
     public TextMeshProUGUI scoreText;
     public int score;
+    private int scoreCount = 100;
 
     public void ScorePellet()
     {
         score += 10;
         scoreText.text = score.ToString();
+        ScoreChecker();
     }
 
     public void ScorePowerPellet()
     {
         score += 50;
         scoreText.text = score.ToString();
+        ScoreChecker();
     }
 
     public void ScoreGhost()
     {
         score += 100;
         scoreText.text = score.ToString();
+        ScoreChecker();
+    }
+
+    public void ScoreChecker()
+    {
+        if (score >= scoreCount)
+        {
+            FindObjectOfType<AdvertSpawner>().SpawnAdvert();
+            scoreCount += 100;
+        }
     }
 }
